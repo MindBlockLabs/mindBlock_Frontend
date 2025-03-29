6c384393837f5b2462bf98c8b2adc2a715631a30
lmontero18 <lmonterom879@gmail.com>
2025-03-29 17:08:54 -0600

    feat(ui): adding glassInput component

--- mindblock/src/components/GameFeatures.tsx
+++ mindblock/src/components/GameFeatures.tsx
@@ -1,9 +1,13 @@
 import { Brain, Trophy, Code } from "lucide-react";
 import { GameFeatureCard } from "../ui/GameFeatureCard";
+import { GlassInput } from "./atoms/GlassInput/GlassInput";
 
 function GameFeature() {
+  const doSomething = () => {
+    console.log("first");
+  };
 
-  //game feature data 
+  //game feature data
   const features = [
     {
       icon: Brain,
@@ -26,13 +30,12 @@ function GameFeature() {
   ];
 
   return (
-    <div className='min-h-screen bg-gradient-to-br from-gray-900 to-black text-white'>
-      <div className='container mx-auto px-4 py-16'>
-        <h2 className='text-3xl font-bold text-center mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500'>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
+      <div className="container mx-auto px-4 py-16">
+        <h2 className="text-3xl font-bold text-center mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
           Game Features
         </h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-          
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {features.map((feature, index) => (
             <GameFeatureCard
               key={index}
@@ -42,6 +45,16 @@ function GameFeature() {
             />
           ))}
         </div>
+      </div>
+      <div className="flex items-center justify-center mt-50">
+        <GlassInput
+          id="Username"
+          label="Username"
+          onChange={doSomething}
+          value=""
+          type="email"
+          placeholder="Choose a username"
+        />
       </div>
     </div>
   );
