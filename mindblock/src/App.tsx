fb049888a07be8f068f417defaccfbcc627527a5
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-04-24 10:13:35 +0100

    modification

--- mindblock/src/App.tsx
+++ mindblock/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <div className="pt-16 min-h-screen bg-[#171717] relative overflow-hidden">
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white overflow-hidden relative">
         <section className="">
           {/* Background gradient lines */}
           <div className="absolute inset-0 bg-[linear-gradient(to_right,#121212_2px,transparent_1px),linear-gradient(to_bottom,#121212_2px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
@@ -38,7 +38,6 @@ function App() {
             </motion.div>
           </div>
         </section>
-
         <GameFeature />
       </div>
     </>
--- mindblock/src/components/GameFeatures.tsx
+++ mindblock/src/components/GameFeatures.tsx
@@ -1,6 +1,5 @@
 import { Brain, Trophy, Code } from "lucide-react";
 import { GameFeatureCard } from "../ui/GameFeatureCard";
-import { GlassInput } from "./atoms/GlassInput/GlassInput";
 
 function GameFeature() {
   //game feature data
@@ -26,31 +25,22 @@ function GameFeature() {
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
-      <div className="container mx-auto px-4 py-16">
-        <h2 className="text-3xl font-bold text-center mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
-          Game Features
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {features.map((feature, index) => (
-            <GameFeatureCard
-              key={index}
-              icon={feature.icon}
-              title={feature.title}
-              description={feature.description}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="flex items-center justify-center mt-50">
-        <GlassInput
-          id="Username"
-          label="Username"
-          onChange={doSomething}
-          value=""
-          type="email"
-          placeholder="Choose a username"
-        />
+    <div className="container mx-auto px-4 py-16">
+      <h2
+        className="text-4xl font-bold text-center mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500"
+        style={{ fontFamily: "var(--font-primary)" }}
+      >
+        Game Features
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {features.map((feature, index) => (
+          <GameFeatureCard
+            key={index}
+            icon={feature.icon}
+            title={feature.title}
+            description={feature.description}
+          />
+        ))}
       </div>
     </div>
   );
